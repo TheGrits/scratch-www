3b36245431ce7517184cde0f38f2e52216168b3e
The_Grits <gritsprogramming@gmail.com>
2016-05-10 21:05:40 -0400

    barebones

---
+++ src/views/privacypolicy/privacypolicy.jsx
@@ -0,0 +1,16 @@
+var React = require('react');
+var FormattedMessage = require('react-intl').FormattedMessage;
+var render = require('../../lib/render.jsx');
+var Page = require('../../components/page/www/page.jsx');
+var Box = require('../../components/box/box.jsx');
+
+require('./privacypolicy.scss');
+
+var Privacypolicy = React.createClass({
+    type: 'Privacypolicy',
+    render: function () {
+        return (
+            
+        );
+    }
+)};
