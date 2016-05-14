fffec30819287f6aab295be41326cec769c69994
The_Grits <gritsprogramming@gmail.com>
2016-05-13 22:38:14 -0400

    Create privacypolicy.jsx

---
+++ src/views/privacypolicy/privacypolicy.jsx
@@ -0,0 +1,14 @@
+var React = require('react');
+var FormattedMessage = require('react-intl').FormattedMessage;
+var render = require('../../lib/render.jsx');
+var Page = require('../../components/page/www/page.jsx');
+var Box = require('../../components/box/box.jsx');
+
+var Privacypolicy = React.createClass({
+    type: 'Privacypolicy',
+    render: function () {
+        return (
+        
+        );
+    }
+});
