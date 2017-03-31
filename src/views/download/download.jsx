a0dda6e313105be3617fdeb2a314c432163301b7
The_Grits <TheGrits@users.noreply.github.com>
2017-03-31 19:47:50 -0400

    Update download.jsx

--- src/views/download/download.jsx
+++ src/views/download/download.jsx
@@ -6,3 +6,24 @@ var FormattedMessage = require('react-intl').FormattedMessage;
 
 var Page = require('../../../components/page/www/page.jsx');
 var InformationPage = require('../../../components/informationpage/informationpage.jsx');
+var TitleBanner = require('../../components/title-banner/title-banner.jsx');
+
+var Download = React.createClass({
+    type: 'Download',
+    render: function () {
+        return (
+            <div className="download">
+                <TitleBanner className="masthead">
+                    <div className="inner">
+                        <h1 className="title-banner-h1">
+                            <FormattedMessage id='download.title' />
+                        </h1>
+                        <p className="title-banner-p intro">
+                            <FormattedMessage id='download.intro' />
+                        </p>
+                    </div>
+                </TitleBanner>
+            </div>
+        );
+    }
+})
