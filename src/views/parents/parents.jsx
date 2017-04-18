b83095adbe232e82f4d79d686107b1e8ae1f3eef
The_Grits <TheGrits@users.noreply.github.com>
2017-04-18 00:03:22 -0400

    Barebones

--- src/views/parents/parents.jsx
+++ src/views/parents/parents.jsx
@@ -1,1 +1,17 @@
+var React = require('react');
+var render = require('../../lib/render.jsx');
 
+var FormattedHTMLMessage = require('react-intl').FormattedHTMLMessage;
+var FormattedMessage = require('react-intl').FormattedMessage;
+
+var Page = require('../../components/page/www/page.jsx');
+var FlexRow = require('../../components/flex-row/flex-row.jsx');
+var SubNavigation = require('../../components/subnavigation/subnavigation.jsx');
+var TitleBanner = require('../../components/title-banner/title-banner.jsx');
+
+require('./parents.scss');
+
+var Parents = React.createClass({
+  
+});
+render(<Page><Parents /></Page>, document.getElementById('app'));
