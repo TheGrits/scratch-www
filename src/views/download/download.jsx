d362b58d842da11477d164dc0710eebc3935299c
The_Grits <TheGrits@users.noreply.github.com>
2017-05-20 00:23:06 -0400

    Update download.jsx

--- src/views/download/download.jsx
+++ src/views/download/download.jsx
@@ -92,52 +92,56 @@ var Download = React.createClass({
                                 <div className="installation-column">
                                     <h3><FormattedMessage id='download.offlineEditorTitle' /></h3>
                                     <p><FormattedMessage id='download.offlineEditorBody' /></p>
-                                    <p>
-                                        <FormattedMessage id='download.macOSX' /> -
-                                        {' '}<a href="http://get.adobe.com/air/">
-                                            <FormattedMessage id='download.download' />
-                                        </a>
-                                    </p>
-                                    <p>
-                                        <FormattedMessage id='download.macOlder' /> -
-                                        {' '}<a href="http://airdownload.adobe.com/air/mac/download/2.6/AdobeAIR.zip">
-                                            <FormattedMessage id='download.download' />
-                                        </a>
-                                    </p>
-                                    <p>
-                                        <FormattedMessage id='download.windows' /> -
-                                        {' '}<a href="http://get.adobe.com/air/">
-                                            <FormattedMessage id='download.download' />
-                                        </a>
-                                    </p>
-                                    <p>
-                                        <FormattedMessage id='download.linux' /> -
-                                        {' '}<a href="http://airdownload.adobe.com/air/lin/download/2.6/AdobeAIRInstaller.bin">
-                                            <FormattedMessage id='download.download' />
-                                        </a>
-                                    </p>
+                                    <ul className="installation-downloads">
+                                        <li className="installation-downloads-item">
+                                            <FormattedMessage id='download.macOSX' /> -
+                                            {' '}<a href="http://get.adobe.com/air/">
+                                                <FormattedMessage id='download.download' />
+                                            </a>
+                                        </li>
+                                        <li className="installation-downloads-item">
+                                            <FormattedMessage id='download.macOlder' /> -
+                                            {' '}<a href="http://airdownload.adobe.com/air/mac/download/2.6/AdobeAIR.zip">
+                                                <FormattedMessage id='download.download' />
+                                            </a>
+                                        </li>
+                                        <li className="installation-downloads-item">
+                                            <FormattedMessage id='download.windows' /> -
+                                            {' '}<a href="http://get.adobe.com/air/">
+                                                <FormattedMessage id='download.download' />
+                                            </a>
+                                        </li>
+                                        <li className="installation-downloads-item">
+                                            <FormattedMessage id='download.linux' /> -
+                                            {' '}<a href="http://airdownload.adobe.com/air/lin/download/2.6/AdobeAIRInstaller.bin">
+                                                <FormattedMessage id='download.download' />
+                                            </a>
+                                        </li>
+                                    </ul>
                                 </div>
                                 <div className="installation-column">
                                     <h3><FormattedMessage id='download.supportMaterialsTitle' /></h3>
                                     <p><FormattedMessage id='download.supportMaterialsBody' /></p>
-                                    <p>
-                                        <FormattedMessage id='download.starterProjects' /> -
-                                        {' '}<a href="https://scratch.mit.edu/scratchr2/static/sa/Scratch2StarterProjects.zip">
-                                            <FormattedMessage id='download.download' />
-                                        </a>
-                                    </p>
-                                    <p>
-                                        <FormattedMessage id='download.gettingStarted' /> -
-                                        {' '}<a href="https://cdn.scratch.mit.edu/scratchr2/static/__709da8e5f3d72129538a4ccdbcbf5f2a__/pdfs/help/Getting-Started-Guide-Scratch2.pdf">
-                                            <FormattedMessage id='download.download' />
-                                        </a>
-                                    </p>
-                                    <p>
-                                        <FormattedMessage id='download.scratchCards' /> -
-                                        {' '}<a href="https://cdn.scratch.mit.edu/scratchr2/static/__709da8e5f3d72129538a4ccdbcbf5f2a__/pdfs/help/Scratch2Cards.pdf">
-                                            <FormattedMessage id='download.download' />
-                                        </a>
-                                    </p>
+                                    <ul className="installation-downloads">
+                                        <li className="installation-downloads-item">
+                                            <FormattedMessage id='download.starterProjects' /> -
+                                            {' '}<a href="https://scratch.mit.edu/scratchr2/static/sa/Scratch2StarterProjects.zip">
+                                                <FormattedMessage id='download.download' />
+                                            </a>
+                                        </li>
+                                        <li className="installation-downloads-item">
+                                            <FormattedMessage id='download.gettingStarted' /> -
+                                            {' '}<a href="https://cdn.scratch.mit.edu/scratchr2/static/__709da8e5f3d72129538a4ccdbcbf5f2a__/pdfs/help/Getting-Started-Guide-Scratch2.pdf">
+                                                <FormattedMessage id='download.download' />
+                                            </a>
+                                        </li>
+                                        <li className="installation-downloads-item">
+                                            <FormattedMessage id='download.scratchCards' /> -
+                                            {' '}<a href="https://cdn.scratch.mit.edu/scratchr2/static/__709da8e5f3d72129538a4ccdbcbf5f2a__/pdfs/help/Scratch2Cards.pdf">
+                                                <FormattedMessage id='download.download' />
+                                            </a>
+                                        </li>
+                                    </ul>
                                 </div>
                             </FlexRow>
                         </section>
